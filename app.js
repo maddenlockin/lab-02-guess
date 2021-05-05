22b35143d7af5db717dccfc7fbfcc89d9ea8ea02
maddenlockin <maddenlockin@gmail.com>
2021-05-04 19:09:41 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // import functions and grab DOM elements
-
+import {  } from 'utils.js'
 // initialize state
-
+let guess = 0;
 // set event listeners to update state and DOM
+const button = document.getElementById('guess-button');
+const userGuess = document.getElementById('number-input');
+const imgResult = document.getElementById('feedback-img');
+const divResult = document.getElementById('feedback-div');
+
+button.addEventListener('click', () => {
+
+
+    
+})
